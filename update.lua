8f18ddcfe45da6dd6b4e75c5848577eb13f46d1d
Taso <Taso@projecterror.dev>
2021-03-31 17:07:19 -0400

    tweak(update): Tweaked update script colors
    This adheres to FiveM's color standards and not ANSI color escape codes, allowing for correct display in svgui

--- update.lua
+++ update.lua
@@ -1,6 +1,7 @@
-Citizen.CreateThread(function()
+CreateThread(function()
 	local updatePath = '/TasoOneAsia/t-notify'
-	local resourceName = "\x1b[32m["..GetCurrentResourceName().."]\x1b[0m"
+	local resourceName = "^2"..GetCurrentResourceName().."]^0"
+
 	function checkVersion(err,responseText, headers)
 		local curVersion = LoadResourceFile(GetCurrentResourceName(), "version")
 		if responseText == nil or curVersion == nil then
@@ -10,10 +11,11 @@ Citizen.CreateThread(function()
 			print("\n" ..resourceName.." is outdated. \nThe latest stable version is "..responseText..", your version is ("..curVersion..")\n\nPlease download the latest stable build from https://github.com/TasoOneAsia/t-notify/")
 			print("\n###############################")
 		elseif(tonumber(curVersion) > tonumber(responseText)) then
-			print("You somehow skipped a few versions of "..resourceName.." or GitHub went offline, if GitHub is still online please try and update your resource")
+			print("You may be using a pre-release of "..resourceName.." or GitHub went offline")
 		else
 			print(resourceName.. '(v' .. responseText .. ") is up to date and has started")
 		end
 	end
+
 	PerformHttpRequest("https://raw.githubusercontent.com"..updatePath.."/master/version", checkVersion, "GET")
 end)
